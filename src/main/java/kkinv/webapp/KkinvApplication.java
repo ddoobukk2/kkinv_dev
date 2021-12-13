f958f4de84b064e7140ec734d12eed3f512ee2c2
ddoobukk2 <ddoobukk2@naver.com>
2021-12-13 23:28:41 +0900

    1

---
+++ src/main/java/kkinv/webapp/KkinvApplication.java
@@ -0,0 +1,13 @@
+package kkinv.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkinvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkinvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kkinv/webapp/KkinvApplicationTests.java
@@ -0,0 +1,13 @@
+package kkinv.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkinvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
